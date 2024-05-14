59bb498ed91f992082114b4f0b6dbcf3969ae031
Jhoan Sebastian Sierra Perdomo <jhoa.sierra@udla.edu.co>
2024-05-14 11:16:39 -0500

    Agregar archivos de proyecto.

---
+++ Data/Conexion.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ProyectoAdmin_EmisoraCristalina.Data
+{
+
+    public class Conexion
+    {
+        protected MySqlConnection? connection;
+
+        protected void Conectar()
+        {
+            try
+            {
+                connection = new MySqlConnection("Server=localhost;Port=3306;Database=Emisora_Cristalina;Uid=root;Pwd=2002");
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        protected void Desconectar()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoAdmin_EmisoraCristalina.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
